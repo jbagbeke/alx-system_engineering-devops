a095da597234b7347204365fb247505e88d4afc6
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-28 01:36:35 +0000

    Puppet mandatory task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Creates a manifest that kills a process named killmenow
+
+exec { 'killmenow':
+  command => '/usr/bin/pkill -f killmenow',
+  path    => '/usr/bin',
+  onlyif  => '/usr/bin/pgrep -f killmenow',
+}
