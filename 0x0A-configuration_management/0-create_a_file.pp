03958b5b6952056d42e93701584d2d649890ab08
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-28 00:41:27 +0000

    Puppet task file deletion

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# /etc/puppet/manifests/create_file.pp
-
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
