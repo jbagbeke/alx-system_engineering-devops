2684b29d3f5194311ca56a8bff04b716b753d1b9
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-17 01:43:55 +0000

    0x16. API advanced Mandatory task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,16 +9,19 @@ def number_of_subscribers(subreddit):
     """
         Queries REDDIT API and returns # of subscribers with given subreddit
                                                                         """
-    if not subreddit:
+
+    try:
+        query_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+        response = requests.get(query_url, allow_redirects=False)
+
+        if 300 <= response.status_code < 400:
+            return 0
+
+        if response.status_code == 200:
+            response_data = response.json()
+            return response_data['data']['subscribers']
+
         return 0
-
-    query_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    response = requests.get(query_url, allow_redirects=False)
-
-    if 300 <= response.status_code < 400 or response.status_code != 200:
+    except Exception:
         return 0
-
-    response_data = response.json()
-
-    return response_data['data']['subscribers']
