9c9f8fb3155cce1945686fdb348e672af575f330
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-17 01:34:04 +0000

    0x16. API advanced Mandatory task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,15 @@
     A function that queries the Reddit API and returns the # of subscribers
                                                                         """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """
         Queries REDDIT API and returns # of subscribers with given subreddit
                                                                         """
+    if not subreddit:
+        return 0
+
     query_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     response = requests.get(query_url, allow_redirects=False)
@@ -18,5 +20,5 @@ def number_of_subscribers(subreddit):
         return 0
 
     response_data = response.json()
-    print(response_data)
+
     return response_data['data']['subscribers']
