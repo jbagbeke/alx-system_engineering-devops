cd0408ee1e799bb62504abf31aaee4e4976cad0d
Joshua Bubune <joshua.legithive@gmail.com>
2024-01-17 01:53:54 +0000

    0x16. API advanced Mandatory task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,10 +17,9 @@ def number_of_subscribers(subreddit):
 
         response = requests.get(query_url)
 
-
         if response.status_code == 200:
             response_data = response.json()
-            return response_data['data']['subscribers']
+            return response_data.get('data').get('subscribers')
 
         return 0
     except Exception:
